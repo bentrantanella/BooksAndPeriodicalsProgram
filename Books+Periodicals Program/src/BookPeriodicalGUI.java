f09af40498a1930fa500cb8b21519bae7a1de599
20btrantanella <20btrantanella@WA-248-9579.schools.westford-ma.gov>
2020-01-08 12:45:42 -0500

    1/8

--- Books+Periodicals Program/src/BookPeriodicalGUI.java
+++ Books+Periodicals Program/src/BookPeriodicalGUI.java
@@ -2,5 +2,14 @@ import javax.swing.*;
 import BreezySwing.*;
 
 public class BookPeriodicalGUI extends GBFrame {
+	
+	public static void main(String[] args) {
+		JFrame frm = new BookPeriodicalGUI();
+		frm.setTitle("Books and Periodicals");
+		frm.setSize(300, 300);
+		frm.setVisible(true);
+	}
+	
+	
 
 }
